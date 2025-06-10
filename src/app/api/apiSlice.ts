dc9a138911058c0289a3d36613c43423ae3fe8fb
SoftDev-Abay <abay.aliev.03@gmail.com>
2025-06-10 20:38:02 +0500

    added tags, used consts from env

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -11,7 +11,8 @@ const redirectToLogin = () => {
 };
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/rest",
+  // baseUrl: "http://localhost:8080/rest",
+  baseUrl: import.meta.env.VITE_BACKEND_URL,
   // baseUrl: "http://localhost:8080/api/v1",
   // credentials: "include", // allow sending cookies (refresh token)
   prepareHeaders: (headers, { getState }) => {
@@ -21,7 +22,7 @@ const baseQuery = fetchBaseQuery({
 
     if (token && token !== "undefined") {
       headers.set("Authorization", `Bearer ${token}`);
-      console.log("we set token on authorization ", token)
+      console.log("we set token on authorization ", token);
     }
 
     return headers;
@@ -36,28 +37,28 @@ const baseQueryWithAuthHandling = async (
   let result = await baseQuery(args, api, extraOptions);
 
   // Check for expired/invalid access token (unauthorized)
-  // if (result?.error?.status === 401) {
-  //   console.warn("Access token expired or invalid. Attempting refresh...");
+  if (result?.error?.status === 401) {
+    // console.warn("Access token expired or invalid. Attempting refresh...");
 
-  //   // Attempt to refresh the token using the secure cookie
-  //   const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+    // Attempt to refresh the token using the secure cookie
+    // const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
 
-  //   if (refreshResult.data) {
-  //     const refreshData = refreshResult.data as { access_token: string };
+    // if (refreshResult.data) {
+    //   const refreshData = refreshResult.data as { access_token: string };
 
-  //     // Save new access token to Redux
-  //     api.dispatch(setCredentials({ token: refreshData.access_token }));
+    //   // Save new access token to Redux
+    //   api.dispatch(setCredentials({ token: refreshData.access_token }));
 
-  //     // Retry original request
-  //     result = await baseQuery(args, api, extraOptions);
-  //   } else {
-  //     // If refresh also fails, log out
-  //     console.error("Refresh token invalid or expired. Logging out.");
-  //     api.dispatch(logOut());
-  //     redirectToLogin();
-  //     return refreshResult;
-  //   }
-  // }
+    //   // Retry original request
+    //   result = await baseQuery(args, api, extraOptions);
+    // } else {
+    // If refresh also fails, log out
+    console.error("Refresh token invalid or expired. Logging out.");
+    api.dispatch(logOut());
+    redirectToLogin();
+    // return refreshResult;
+    // }
+  }
 
   // If authenticated but access is denied due to role/permissions
   if (result?.error?.status === 403) {
@@ -74,5 +75,5 @@ const baseQueryWithAuthHandling = async (
 export const apiSlice = createApi({
   baseQuery: baseQueryWithAuthHandling,
   endpoints: () => ({}),
-  tagTypes: ["Club", "Event", "Post"],
+  tagTypes: ["Club", "Event", "Post", "Comment", "ClubMember"],
 });
